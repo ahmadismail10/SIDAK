c5f3ac3961989e045a09ae71ef01af25f9badfaf
Muhsin <muhsin21baf@hotmail.com>
2017-07-12 14:00:01 +0700

    first commit

---
+++ php/uploaddb.php
@@ -0,0 +1,5 @@
+<form action="php/unggah.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload" name="submit">
+</form>
